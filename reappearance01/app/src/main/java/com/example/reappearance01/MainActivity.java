6b88da96d63c5de575ee4cb8f39689518ee9384f
GihyeonKim2019 <odohah@kaist.ac.kr>
2022-07-16 21:26:38 +0900

    secondcommit

--- reappearance01/app/src/main/java/com/example/reappearance01/MainActivity.java
+++ reappearance01/app/src/main/java/com/example/reappearance01/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.reappearance01;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Hello");
     }
 }
