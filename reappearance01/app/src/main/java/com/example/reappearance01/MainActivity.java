2239130e7b5a41df3b782b85f1f6f4246a178e57
JajunHan <hanjj03@naver.com>
2022-07-17 20:25:18 +0900

    서버와 통신하고, 정보를 받아옴. 또한, walking_speed를 설정하면 그에 따른 결과(대중교통 대기시간)가 나오게함.

--- reappearance01/app/src/main/java/com/example/reappearance01/MainActivity.java
+++ reappearance01/app/src/main/java/com/example/reappearance01/MainActivity.java
@@ -4,15 +4,50 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "Hello");
+        search = (ImageButton) findViewById(R.id.search_result);
+
+        //검색버튼
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // intent로 다른 화면 띄위게 해주면 됨
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
